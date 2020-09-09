45b5ddb8daba3c84dd4d4e484ff248628dcf681c
Vincent Robinson <59773500+Varobinson@users.noreply.github.com>
2020-09-09 15:10:15 -0400

    added links to dom

--- scripts/index.js
+++ scripts/index.js
@@ -6,3 +6,23 @@ const addresses = [
     "https://duckduckgo.com"
 ];
 
+for(let i = 0; i < addresses.length; i++){
+    const link = document.createElement('a');
+    link.setAttribute('href', addresses[i]);
+    link.textContent = addresses[i];
+    const pageLink = document.querySelector('.js-link-container')
+    pageLink.appendChild(link)
+}
+            
+// const link2 = document.createElement('a');
+// link2.setAttribute('href', addresses[1]);
+// link2.textContent = addresses[1];
+// const pageLink2 = document.querySelector('.js-link-container')
+// pageLink2.appendChild(link2)
+
+// const link3 = document.createElement('a');
+// link3.setAttribute('href', addresses[2]);
+// link3.textContent = addresses[2];
+// const pageLink3 = document.querySelector('.js-link-container')
+// pageLink3.appendChild(link3)
+
